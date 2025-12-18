b83d0f404eebc47f536178f73ddf850ae05234a0
no2ehi <ruholahn2@gmail.com>
2025-12-18 08:24:26 +0330

    feat: set up Nuxt pages and layouts
    - Configure file-based routing with pages
    - Add default layout
    - Set up product routes (list, detail, create)
    - Remove old app.vue in favor of Nuxt 3 structure

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import AppHeader from "~/components/layout/AppHeader.vue";
+import AppFooter from "~/components/layout/AppFooter.vue";
+</script>
+
+<template>
+  <div>
+    <!-- Skip Navigation Link for keyboard and screen reader users -->
+    <a
+      href="#main-content"
+      class="sr-only focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50 focus:rounded-md focus:bg-blue-600 focus:px-4 focus:py-2 focus:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+    >
+      Skip to main content
+    </a>
+
+    <AppHeader />
+
+    <main id="main-content" class="mx-auto sm:container">
+      <slot />
+    </main>
+
+    <AppFooter />
+  </div>
+</template>
